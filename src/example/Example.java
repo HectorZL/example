b1a20e6604877e7a35fcd6a14358d8a5be310c1d
Hector Zambrano <zambranohector2002@gmail.com>
2023-05-10 06:16:01 -0500

    <fix>(<main>): <change the gitignore and fix my name>
    <body>
    
    <footer>
    
    By HectorZL

---
+++ src/example/Example.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package example;
+
+/**
+ *
+ * @author Hector Zambrano 
+ */
+public class Example {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+      System.out.println("Hello World!");
+      System.out.println("My name is Hector and I'm 21 years old");
+    }
+    
+}
